296c400bdb5b967ed0dc6b2af9111779ee88b220
pbcpraveen <pbcpraveen@gmail.com>
2024-02-13 16:12:02 -0800

    Restructuring tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -3,115 +3,73 @@ import org.junit.jupiter.api.Test;
 import org.w3c.dom.Node;
 import helper.XMLParser;
 
+import java.util.List;
+
 public class IntegrationTest {
 
+    static String expectedResultsPath = "src/test/resources/expected/";
+    static String testResultsPath = "src/test/resources/results/";
+    static String testQueryPath = "src/test/resources/query/";
+    static List<String> queryNames = List.of("query1", "query2", "query3", "query4", "query5", "query6", "query7", "query8", "query9", "query10");
+    static List<String> resultNames = List.of("result1", "result2", "result3", "result4", "result5", "result6", "result7", "result8", "result9", "result10");
+
+    private static boolean runAndCompare(int index){
+        String queryFileName = testQueryPath + queryNames.get(index) + ".txt";
+        String outputFileName = testResultsPath + resultNames.get(index) + ".xml";
+        String expectedFileName = expectedResultsPath + resultNames.get(index) + ".xml";
+        Runner.main(new String[]{queryFileName, outputFileName});
+        Node xml1 = new XMLParser(outputFileName).root;
+        Node xml2 = new XMLParser(expectedFileName).root;
+
+        return FilterFunctions.isValueEqual(xml1, xml2);
+    }
     @Test
     public void testQuery1(){
-        String queryFileName = "src/main/resources/query/query1.txt";
-        String outputFileName = "src/main/resources/test/results/result1.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result1.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result1.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(0);
     }
 
     @Test
     public void testQuery2(){
-        String queryFileName = "src/main/resources/query/query2.txt";
-        String outputFileName = "src/main/resources/test/results/result2.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result2.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result2.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(1);
     }
 
     @Test
     public void testQuery3(){
-        String queryFileName = "src/main/resources/query/query3.txt";
-        String outputFileName = "src/main/resources/test/results/result3.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result3.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result3.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(2);
     }
 
     @Test
     public void testQuery4(){
-        String queryFileName = "src/main/resources/query/query4.txt";
-        String outputFileName = "src/main/resources/test/results/result4.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result4.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result4.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(3);
     }
 
     @Test
     public void testQuery5(){
-        String queryFileName = "src/main/resources/query/query5.txt";
-        String outputFileName = "src/main/resources/test/results/result5.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result5.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result5.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(4);
     }
 
     @Test
     public void testQuery6(){
-        String queryFileName = "src/main/resources/query/query6.txt";
-        String outputFileName = "src/main/resources/test/results/result6.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result6.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result6.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(5);
     }
 
     @Test
     public void testQuery7(){
-        String queryFileName = "src/main/resources/query/query7.txt";
-        String outputFileName = "src/main/resources/test/results/result7.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result7.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result7.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(6);
     }
 
     @Test
     public void testQuery8(){
-        String queryFileName = "src/main/resources/query/query8.txt";
-        String outputFileName = "src/main/resources/test/results/result8.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result8.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result8.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(7);
     }
 
     @Test
     public void testQuery9(){
-        String queryFileName = "src/main/resources/query/query9.txt";
-        String outputFileName = "src/main/resources/test/results/result9.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result9.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result9.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(8);
     }
 
     @Test
     public void testQuery10(){
-        String queryFileName = "src/main/resources/query/query10.txt";
-        String outputFileName = "src/main/resources/test/results/result10.xml";
-        Runner.main(new String[]{queryFileName, outputFileName});
-        Node xml1 = new XMLParser("src/main/resources/test/results/result10.xml").root;
-        Node xml2 = new XMLParser("src/main/resources/results/result10.xml").root;
-
-        assert  FilterFunctions.isValueEqual(xml1, xml2);
+        assert runAndCompare(9);
     }
 }
